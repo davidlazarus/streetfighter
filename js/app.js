d5ee1d15fed0f6167275f022a5c6c06c8b0380df
davidlazarus <davidlazarus89@gmail.com>
2016-07-02 18:37:23 +0200

    2.7.16

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+
+	.mouseleave(function() {
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+	})
+
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+
+	.mouseup(function() {
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+	});
+
+
+
+
+});
+
+
