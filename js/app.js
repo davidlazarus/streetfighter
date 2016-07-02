dba5a31acb90492fe19856960e195eed98e677e5
davidlazarus <davidlazarus89@gmail.com>
2016-07-02 19:12:12 +0200

    2.7.16

--- js/app.js
+++ js/app.js
@@ -11,9 +11,18 @@ $(document).ready(function() {
 	})
 
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1200px'},
+			500,
+			function() {
+			$(this).hide();
+			$(this).css('left', '540px');
+			});
+
 	})
 
 	.mouseup(function() {
@@ -21,9 +30,10 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 	});
 
+});	
 
-
-
-});
-
-
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();	
+}
