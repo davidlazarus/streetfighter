5e51c79074aa23d831450c9c9724c04be3bd3031
davidlazarus <davidlazarus89@gmail.com>
2016-07-03 17:06:22 +0200

    last

--- js/app.js
+++ js/app.js
@@ -87,11 +87,11 @@ $(document).ready(function() {
 			$('.hulk-ryu-ready').show();
 	}});
 
-})
+});
 
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();	
-}
+};
